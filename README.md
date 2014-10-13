78b2854ecdb8992a98ac144e68873ffe0dfa00e6
Andrey Sizov <andrey.sizov@jetbrains.com>
2014-10-14 02:23:31 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+docker-centos6-buildagent
+=====================
+
+This is the Docker container with JetBrains [`TeamCity`](https://www.jetbrains.com/teamcity/) Build Agent inside.
+Just run it and authorize the new agent in `Unauthorized agents` tab.
+
+### Environment variables
+* `TC_URL` - URL to your TeamCity installation like `http://teamcity.yourdomain.com` w/o trailing slash. 
+* `TOKEN` -  Authorization token of your Build Agent, if you want to run your build agent again with all settings preserved. Optional.
+
---
+++ run-services.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+rccheck() {
+   if [[ $? != 0 ]]; then
+      echo "Error! Stopping the script."
+      exit 1
+   fi
+}
+
+if [ "${TC_URL}" ]; then
+   echo "TeamCity URL is provided: ${TC_URL}"
+else
+   TC_URL="http://buildserver.labs.intellij.net/"
+   echo "TeamCity URL is not provided, using default: ${TC_URL}"
+fi
+
+wget -O /home/buildagent/buildAgent.zip ${TC_URL}/update/buildAgent.zip; rccheck
+
+if [ ${TOKEN} ]; then
+    sed -i "s/authorizationToken=.*/authorizationToken=${TOKEN}/" /home/buildagent/buildAgent/conf/buildAgent.properties
+fi
+
+unzip -q -d /home/buildagent/buildAgent /home/buildagent/buildAgent.zip; rccheck
+chmod +x /home/buildagent/buildAgent/bin/*.sh
+
+cp -p /home/buildagent/buildAgent/conf/buildAgent.dist.properties /home/buildagent/buildAgent/conf/buildAgent.properties
+sed -i "s|serverUrl=.*|serverUrl=${TC_URL}|" /home/buildagent/buildAgent/conf/buildAgent.properties; rccheck
+
+
+/home/buildagent/buildAgent/bin/agent.sh start
+
+while [ ! -f /home/buildagent/buildAgent/logs/teamcity-agent.log ]; 
+do 
+   echo -n "."
+   sleep 1
+done
+
+trap "/home/buildagent/buildAgent/bin/agent.sh stop" SIGINT SIGTERM SIGHUP
+
+tail -qf /home/buildagent/buildAgent/logs/teamcity-agent.log &
+wait
